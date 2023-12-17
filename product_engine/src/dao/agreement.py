698dc6e1e6e79f1b793ac6b0e20e7a07d3eed064
kr1sta1l <dmitriy_2003s@mail.ru>
2023-12-18 02:53:43 +0300

    Added agreement DAO

---
+++ product_engine/src/dao/agreement.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DECIMAL, TIMESTAMP
+from sqlalchemy.types import DateTime
+
+from controllers.session import Base
+
+
+class AgreementDao(Base):
+    __tablename__ = "agreement"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_code = Column(String, index=True)
+    client_id = Column(Integer, index=True)
+    create_datetime = Column(DateTime(timezone=True))
+    status = Column(Integer)
+    principle_amount = Column(Integer)
+    term = Column(Integer)
+    origination_amount = Column(Integer)
+    interest_rate = Column(DECIMAL)
+    disbursement_datetime = Column(DateTime(timezone=True))
